f39e3aadd7aec332a44d983eb685adfa0991a28d
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-06 00:10:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(arraydwarves)# code an argument here
-  arraydwarves.each.with_index# Your code here
-  puts arraydwarves
+  arraydwarves.each.with_index(1) do |dwarf, index|# Your code here
+  puts "#{index}. #{dwarf}"
+  end
 end
 
 def summon_captain_planet# code an argument here
