9ab889699bf6f70451502d9d964086d724ee38a8
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-06 00:23:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,10 @@ def long_planeteer_calls(calls)# code an argument here
 end
 
 def find_the_cheese(array)# code an argument here
-  array.include(cheese_types)# the array below is here to help
+  if array.include(cheese_types)# the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  true
+else
+  false
 end
+end 
